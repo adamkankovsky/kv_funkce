6e9e622e1f36d24d09abb591be06f01ab36bf4ca
Kaňkovský Adam <kankovsky@SSPTAJI.CZ>
2024-10-07 13:09:42 +0200

    hotova kalkulacka

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ if ('serviceWorker' in navigator) {
     })
 }
 
+function calculate(){
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const c = document.getElementById("c").value;
+
+    let res1;
+    let res2;
+
+    let disc = (b * b) - (4 * a * c)
+
+    if (disc >= 0){
+        if (disc == 0){
+            res1 = (-b) / (2 * a)
+            res2 = (-b) / (2 * a)
+        } else {
+            res1 = (-b + Math.sqrt(disc)) / (2 * a)
+            res2 = (-b - Math.sqrt(disc)) / (2 * a)
+        }
+        } else {
+        res1 = "nelze najit vysledek v poli realnych cisel";
+    }
+
+    document.getElementById('x1').innerText = res1;
+    document.getElementById('x2').innerText = res2;
+}
